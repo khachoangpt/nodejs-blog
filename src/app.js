38c58798f7c0f14e9e118932a539156bf62eabde
Hoang Nguyen <hoang.nguyen@pionero.io>
2023-02-23 23:34:41 +0700

    feat: init files and folder

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet")
+const compression = require("compression")
+const app = express();
+
+//init middleware
+app.use(morgan("combined"));
+app.use(helmet())
+app.use(compression())
+
+//init db
+
+//init router
+const strComp = "Hello Hoang"
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome to nodejs",
+  });
+});
+
+//handle error
+
+module.exports = app;
